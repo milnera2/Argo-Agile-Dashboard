ec346bc42b5b81dd84e2219b7f3ef0bd78447b92
Sam Lunnemann <guitarclassicalman@gmail.com>
2026-04-15 11:28:43 -0400

    Point Login to rest of the current site

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 export default function Login() {
-  return <div className="p-8 text-2xl font-bold">Login here</div>;
+  return <div className="p-8 text-2xl font-bold">
+    <Link to="/dashboard">Login here </Link>
+    </div>;
 }
