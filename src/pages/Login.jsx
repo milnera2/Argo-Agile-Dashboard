66fcc478262c6a245591138190c528b100c11a43
Sam Lunnemann <guitarclassicalman@gmail.com>
2026-04-17 14:10:30 -0400

    Revert "Point Login to rest of the current site"
    This reverts commit ec346bc42b5b81dd84e2219b7f3ef0bd78447b92.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,3 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
 export default function Login() {
-  return <div className="p-8 text-2xl font-bold">
-    <Link to="/dashboard">Login here </Link>
-    </div>;
+  return <div className="p-8 text-2xl font-bold">Login here</div>;
 }
